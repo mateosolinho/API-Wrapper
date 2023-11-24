688445548897414fce6c6343ea57d89c28855ebe
M4teoo <124877302+mateosolinho@users.noreply.github.com>
2023-11-24 19:28:50 +0100

    Delete ForniteAPIWrapper/src/Config.java

--- ForniteAPIWrapper/src/Config.java
+++
@@ -1,5 +0,0 @@
-public class Config {
-    public static final String API_URL = "https://fortnite-api.com/v2/stats/br/v2?name=Simon_Fantasma";
-    public static final String AUTHORIZATION_HEADER = "9e7a2cc6-8e80-4861-a264-ba5ca5338ad2";
-}
-// No mostrar nunca este archivo
